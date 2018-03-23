7822bd61cf91c5b47bda90f91dcc00bcb88196c3
jodidl1998 <jodi.deloof.y0180@student.hogent.be>
2018-03-23 17:02:28 +0100

    linked articles component to data service

--- frontend/src/app/data.service.ts
+++ frontend/src/app/data.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  getArticles() {
+    return this.http.get('http://localhost:3000/articles').map(res => res.json());
+  }
+
+  addArticle(newArticle) {
+    const headers = new Headers();
+    headers.append('Content-Type', 'Application/json');
+    return this.http.post('http://localhost:3000/article/', newArticle, {headers: headers}).map(res => res.json());
+  }
 }
