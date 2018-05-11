1fc3ab30cfbab7ca4a3f41dd3add107f5ff07214
jodidl1998 <jodi.deloof.y0180@student.hogent.be>
2018-05-11 16:15:26 +0200

    environment variable

--- backend/models/User.js
+++ backend/models/User.js
@@ -36,7 +36,7 @@ UserSchema.methods.generateJWT = function() {
       _id: this._id,
       username: this.username,
       exp: parseInt(exp.getTime() / 1000)
-    },"1234"
+    },process.env.RECIPE_BACKEND_SECRET
   );
 };
 
